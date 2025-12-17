0785462b88b8e8fc4d734659e772160609027080
next.js <is.gonzalezp@duocuc.cl>
2025-12-17 15:44:33 +0000

    integracion 3

--- app/components/Batidos.tsx
+++ app/components/Batidos.tsx
@@ -1,13 +1,12 @@
 import CardProduct from "./CardProduct";
 import { batidos } from "../data/batidos";
-import Dolar from "./dolar";
 export default function Batidos({ max }: { max?: number }) {
   const mostrar = max ? batidos.slice(0, max) : batidos;
 
   return (
     <div className="w-full max-w-4xl">
       <h2 className="text-2xl font-bold mb-6 text-center">Batidos</h2>
-      {<Dolar></Dolar>}
+
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
         {mostrar.map((batido) => (
           <CardProduct
